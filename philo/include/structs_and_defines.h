6530d90b57bd20fb3094650df5c597f6292cc1a6
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-02-02 00:44:37 +0100

    Add structures for philo and main program

--- philo/include/structs_and_defines.h
+++ philo/include/structs_and_defines.h
@@ -6,12 +6,15 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/01 22:56:23 by asagymba          #+#    #+#             */
-/*   Updated: 2025/02/02 00:28:27 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/02/02 00:43:52 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef STRUCTS_AND_DEFINES_H
 # define STRUCTS_AND_DEFINES_H
+
+# include <pthread.h>
+# include <stdbool.h>
 
 /**
  * ---------------------------------------------------------------------------
@@ -35,11 +38,9 @@
  * Structures.
  * ---------------------------------------------------------------------------
  */
-/**
- * About time frames: it's not written in the subject, that
- * time_to_die, time_to_eat or time_to_sleep can't be 0.
- * So we treat 0 time frames as valid.
- */
+/* About time frames: it's not written in the subject,
+ * that time_to_die, time_to_eat or time_to_sleep can't be 0.
+ * So we treat 0 time frames as valid. */
 struct s_args
 {
 	int	num_of_philos;
@@ -47,6 +48,29 @@ struct s_args
 	int	time_to_eat;
 	int	time_to_sleep;
 	int	cycles;			/* -1 == not initialized. */
+};
+
+/* We can basically treat philo as an object. Well, that's his body! */
+struct s_philo
+{
+	pthread_t		thread;
+	int				id;				/* For output (logging) increase by 1. */
+	long			last_meal;		/* Time of last meal in milliseconds. */
+	pthread_mutex_t	meal_lock;		/* \ref last_meal is a critical section. */
+	int				meals_eaten;
+	struct s_data	*main_data;
+};
+
+/* Subject doesn't mention that watchdog must be a thread.
+ * However, if it must, we can easily achieve this. */
+struct s_data
+{
+	struct s_args	args;
+	struct s_philo	*philos;
+	long			start_time;
+	pthread_mutex_t	output_lock;
+	pthread_mutex_t	*forks;
+	bool			some_philo_died;
 };
 
 #endif	/* STRUCTS_AND_DEFINES_H */
