ce5bc6b6fdd7eb84edfa3c35ac7475fc2966c858
Asset Sagymbaev <69522068+0x416c6941@users.noreply.github.com>
2025-02-03 01:07:05 +0100

    ft_usleep: Use usleep() to suspend threads execution

--- philo/src/time_stuff.c
+++ philo/src/time_stuff.c
@@ -6,7 +6,7 @@
 /*   By: asagymba <asagymba@student.42prague.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/02 00:50:00 by asagymba          #+#    #+#             */
-/*   Updated: 2025/02/02 15:13:12 by asagymba         ###   ########.fr       */
+/*   Updated: 2025/02/03 01:05:36 by asagymba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,13 @@
 #include <sys/time.h>
 #include <utils.h>
 #include <structs_and_defines.h>
+#include <unistd.h>
 
 #define MS_IN_SEC			1000
 #define MICROSECONDS_IN_MS	1000
+
+/* In microseconds. See usleep(3). */
+#define FT_USLEEP_ADD_DELAY	250
 
 long	ft_get_current_ms(void)
 {
@@ -36,10 +40,14 @@ void	ft_wait_until(long ms)
 		;
 }
 
+/* usleep() syscall actually suspends the execution
+ * of a block of code for the specified amount of seconds.
+ * When we use multiple threads on an actual physical CPU thread,
+ * it proves to be REALLY handy. */
 void	ft_usleep(long ms)
 {
 	const long	start = ft_get_current_ms();
 
 	while ((ft_get_current_ms() - start) < ms)
-		;
+		usleep(FT_USLEEP_ADD_DELAY);
 }
